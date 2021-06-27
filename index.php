58564ad03e561e86fc588a465609762dc7c4d2b6
minsiya325 <77268814+minsiya325@users.noreply.github.com>
2021-06-27 10:46:09 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+
+<html>
+<head>
+ <meta name="viewport" content="width=device-width" content="initial-scale=1">
+ <link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/log.css" rel="stylesheet">
+</head>
+<body id="p3">
+ 
+  <div class="container"  >
+  
+  <div class="col-sm-12 ">
+    
+    <div class="col-sm-6 " style='background-color:#FFFFFF; margin-top:350px;'>
+<h2>Opinion Mining For IPL</h2>
+
+<br>
+<h4>Prediction Result</h4>
+	<?php
+set_time_limit(0);
+	
+
+
+
+$python=`python test.py`;
+echo"<pre>$python</pre>";
+
+	
+	?>
+	</div>
+   </div>
+   </div>
+
+
+</body>
+</html>
